83d65f3399cc847078549cabe66905c76970812e
Arnav 50 <arnavthakur13456@gmail.com>
2026-02-21 18:11:43 +0530

    Adding Assignemnet and airthemtioc operators learning concept

---
+++ javaScript/Chapeter_04_operators/assignedArithematicOperators.js
@@ -0,0 +1,23 @@
+let x = 10; y = 40;
+x += 20;  // Addition 
+
+console.log("value of x is:", x);
+console.log(y - x); // Substraction
+
+c = x * y;
+console.log("multiplication of x and y is :", c);
+
+console.log(x % y); //(modulus - remainder)
+
+d = x / y;
+console.log(d);
+
+console.log(x ** y);  // (Exponentiation) like 30 ^ 40
+
+x++; //increment
+y--; // decrement
+console.log(x, y);
+
+
+
+
