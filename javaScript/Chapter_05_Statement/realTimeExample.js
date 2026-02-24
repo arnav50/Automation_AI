7c3f09d9cf53ae5adff984803433175078477a0a
Arnav 50 <arnavthakur13456@gmail.com>
2026-02-24 13:38:06 +0530

    update the all thing

---
+++ javaScript/Chapter_05_Statement/realTimeExample.js
@@ -0,0 +1,22 @@
+let isloggedIn = true;
+let userRole = "editor";
+
+
+if (isloggedIn) {
+    if (userRole === "admin") {
+        console.log("You can login all and All admin acess you have");
+    }
+
+    else if (userRole === "editor") {
+        console.log("Welcome - you are login as editor");
+
+    }
+
+    else if (userRole === "user") {
+        console.log("welcome - You have login as a user");
+    }
+
+    else {
+        console.log("No Auth access you have for this domain");
+    }
+}
---
+++ javaScript/Chapter_05_Statement/switch.js
@@ -0,0 +1,33 @@
+let day = 6
+
+switch (day) {
+    case 0:
+        console.log("Sunday - Enjoy today is Sunday");
+        break;
+
+    case 1:
+        console.log("Monday - Today is busy schedule");
+        break;
+
+    case 2:
+        console.log("Tuesday - Today is more focus on SDLC process");
+        break;
+
+    case 3:
+        console.log("Wednesday - Today Worked on Development");
+        break;
+
+    case 4:
+        console.log("Thursday - Today Worked on Testing");
+        break;
+
+    case 5:
+        console.log("Friday - Today Worked on Deployment");
+        break;
+
+    case 6:
+        console.log("Saturday - Plan for next week");
+        break;
+
+
+}
