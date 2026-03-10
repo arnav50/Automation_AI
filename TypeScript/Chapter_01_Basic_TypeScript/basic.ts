4c2343469c19779553d61d5b9eea3c9015d186e1
Arnav 50 <arnavthakur13456@gmail.com>
2026-03-10 08:26:04 +0530

    Adding Array concept

---
+++ TypeScript/Chapter_01_Basic_TypeScript/basic.ts
@@ -0,0 +1,19 @@
+//Basic Types
+let message: string = "Welcome to Type Script";
+console.log(message);
+
+//Basic + conditions
+let age: number = 25;
+console.log(typeof age);
+
+if (age > 18) {
+    console.log("Eligible for vote");
+} else {
+    console.log("Not Eligible for vote");
+}
+
+
+let a: number = 10;
+let b: number = 20;
+
+console.log(a + b);
